b0c345ce0db1b8fdd1b1f48d8575a94189d06899
Syn <joel.gonzaga@gmail.com>
2017-09-19 17:04:11 -0700

    added the update thing. Works as expected.

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+/*
+
+Syntax below uses Object Deconstruction.
+Is equivalent to:
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+*/
+const {MongoClient, ObjectID} = require('mongodb');
+
+// For Object Destructuring - takes an object and makes it into variables?
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('unreal').findOneAndUpdate({
+    // Filter Object. Select by this
+    _id: new ObjectID("59c158db2cd8e2187f9a2053")
+  },{
+    // Property object. What to update
+    $set: {
+      comments: "I have been changed"
+    }
+  },{
+    // Options
+    returnOriginal: false
+  }).then((result)=>{
+    console.log(result);
+  })
+
+  db.close();
+});
