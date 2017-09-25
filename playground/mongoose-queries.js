a518f0cf489fe3a4c68d8c590d7830f624b91fef
Syn <joel.gonzaga@gmail.com>
2017-09-25 12:30:40 -0700

    Made some more queires in the playground. Works as expected.

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,8 +1,41 @@
+const { ObjectID } = require('mongodb');
+
 const { mongoose } = require('./../server/db/mongoose');
 const { Todo } = require('./../server/model/todo');
 const { userModel } = require('./../server/model/user');
+/*
+userModel.findOne({
+  userName: "BilboBaggins"
+}).then((user)=>{
 
-var id = "59c94a98bb021110af7cc73d";
+  if (!user){
+    console.log("Empty and void");
+    console.log(user);
+    // Used to break the function only.
+    return null;
+  }
+  console.log("Found this!");
+  console.log(user);
+}).catch((error)=>{
+  console.log('we had an error');
+  console.log(error);
+});
+*/
+userModel.findById("09c5584ca3cea9e8a0342ac7").then((user)=>{
+
+  if (!user){
+    console.log("Empty and void");
+    console.log(user);
+    // Used to break the function only.
+    return null;
+  }
+  console.log("Found this!");
+  console.log(user);
+}).catch((error)=>{
+  console.log('we had an error');
+  // console.log(error);
+});
+/*
 
 Todo.find({
   // Nota Bene: here you can pass on an ID as a string.
@@ -21,3 +54,4 @@ Todo.findOne({
   console.log("Called Find One");
   console.log("Got this.. ",todo);
 });
+*/
